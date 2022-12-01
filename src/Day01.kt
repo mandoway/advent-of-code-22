4318ba26e84a4de6f6a7cdc8dd31a6065844f18b
Markus Bointner <bointner.markus9@gmail.com>
2022-12-01 16:49:39 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
+    fun sumBuckets(input: List<String>): List<Int> {
+        var sum1 = 0
+        return input.fold(emptyList<Int>()) { acc, s ->
+            if (s.isBlank()) {
+                (acc + sum1).also {
+                    sum1 = 0
+                }
+            } else {
+                sum1 += s.toInt()
+                acc
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return sumBuckets(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return sumBuckets(input).sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
