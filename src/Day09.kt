4187082cf4ba4fc4a61f1c3a9c28ef8fb3793cfb
Markus Bointner <bointner.markus9@gmail.com>
2022-12-10 02:04:19 +0100

    day 9 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -12,66 +12,65 @@ operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>) = first + other.first to
 
 fun Pair<Int, Int>.coerceIn(range: IntRange) = first.coerceIn(range) to second.coerceIn(range)
 
-fun main() {
-    fun getMoveVector(direction: String) = when (direction) {
-        "R" -> 1 to 0
-        "L" -> -1 to 0
-        "U" -> 0 to 1
-        "D" -> 0 to -1
-        else -> error("invalid state")
+fun Pair<Int, Int>.shouldMoveTo(other: Pair<Int, Int>) = distanceTo(other) > sqrt(2.0)
+
+fun getMoveVector(direction: String) = when (direction) {
+    "R" -> 1 to 0
+    "L" -> -1 to 0
+    "U" -> 0 to 1
+    "D" -> 0 to -1
+    else -> error("invalid state")
+}
+
+fun Pair<Int, Int>.unitMoveTowards(head: Pair<Int, Int>) = (head - this).coerceIn(-1..1)
+
+fun getTotalTailPositions(input: List<String>, ropeLength: Int = 2): Int {
+    val segments = MutableList(ropeLength) { 0 to 0 }
+    val tailPositions = mutableSetOf(0 to 0)
+
+    input.forEach {
+        val (direction, moves) = it.split(" ")
+
+        val moveVector = getMoveVector(direction)
+
+        repeat(moves.toInt()) {
+            segments[0] += moveVector
+
+            segments
+                .indices
+                .zipWithNext()
+                .forEach { (headIndex, tailIndex) ->
+                    val head = segments[headIndex]
+                    val tail = segments[tailIndex]
+
+                    if (tail.shouldMoveTo(head)) {
+                        segments[tailIndex] += tail.unitMoveTowards(head)
+                    }
+                }
+
+            tailPositions.add(segments.last())
+        }
     }
 
+    return tailPositions.size
+}
+
+
+fun main() {
     fun part1(input: List<String>): Int {
-        var head = 0 to 0
-        var tail = 0 to 0
-        val tailPositions = mutableSetOf(0 to 0)
-
-        input.forEach {
-            val (direction, moves) = it.split(" ")
-
-            val moveVector = getMoveVector(direction)
-
-            repeat(moves.toInt()) {
-                head += moveVector
-
-                if (head.distanceTo(tail) > sqrt(2.0)) {
-                    val tailMove = (head - tail).coerceIn(-1..1)
-                    tail += tailMove
-                    tailPositions.add(tail)
-                }
-            }
-        }
-
-        return tailPositions.size
+        return getTotalTailPositions(input)
     }
 
     fun part2(input: List<String>): Int {
-        var head = 0 to 0
-        var tail = 0 to 0
-        val tailPositions = mutableSetOf(0 to 0)
-
-        input.forEach {
-            val (direction, moves) = it.split(" ")
-
-            val moveVector = getMoveVector(direction)
-
-            repeat(moves.toInt()) {
-                head += moveVector
-
-                if (head.distanceTo(tail) > sqrt(2.0)) {
-                    val tailMove = (head - tail).coerceIn(-1..1)
-                    tail += tailMove
-                    tailPositions.add(tail)
-                }
-            }
-        }
-
-        return tailPositions.size
+        return getTotalTailPositions(input, ropeLength = 10)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
+    val testInput2 = readInput("Day09_test2")
     check(part1(testInput) == 13)
+    check(part2(testInput) == 1)
+    check(part2(testInput2) == 36)
 
     val input = readInput("Day09")
     println(part1(input))
